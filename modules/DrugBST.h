3b167cd43806e954a5f48fd9bf57d1f6cc544bca
maki-ni <nehemiahelias14@gmail.com>
2025-12-24 16:53:50 +0300

    created function to export to file

--- modules/DrugBST.h
+++ modules/DrugBST.h
@@ -1,6 +1,7 @@
 #ifndef DRUGBST_H
 #define DRUGBST_H
 #include <string>
+#include <fstream>
 using namespace std;
 
 struct Drug
@@ -18,12 +19,14 @@ private:
     Drug *insert(Drug *node, string name);
     bool search(Drug *node, string name);
     void inorder(Drug *node);
+    void inorderToFile(Drug *node, ofstream &out);
 
 public:
     DrugBST();
     void addDrug(string name);
     void findDrug(string name);
     void displayDrugs();
+    void exportToFile(const string &filename);
 };
 
 #endif
